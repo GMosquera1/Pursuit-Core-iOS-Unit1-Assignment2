9e1a41030a2320560ed929370996347f2ccb780b
GMosquera1 <genesismosquera@pursuit.org>
2018-10-23 17:42:21 -0400

    Final edit

--- Hangman/Hangman/main.swift
+++ Hangman/Hangman/main.swift
@@ -8,42 +8,189 @@
 
 import Foundation
 
-//print("Hola 😆. Are you ready?")
-//
-//sleep (2)
-//
-//var imageHang = """
-//
-//        -------
-//        T     T
-//              |
-//              |
-//              |
-//              |
-//    ---------------------
-//
-//"""
-//print (imageHang)
-//sleep (3)
+print("Hola 😆. Are you ready?")
+
+sleep (2)
+
+var imageHang = """
+
+    -------
+    T     T
+          |
+          |
+          |
+          |
+---------------------
+
+"""
+var imageHang1 = """
+    -------
+    T     T
+    🧟‍♀️    |
+          |
+          |
+          |
+---------------------
+
+"""
+var imageHang2 = """
+    -------
+    T     T
+    🧟‍♀️    |
+    🤚🏽    |
+          |
+          |
+---------------------
+
+"""
+var imageHang3 = """
+    -------
+    T     T
+    🧟‍♀️    |
+    👋🏽🤚🏽  |
+          |
+          |
+---------------------
+"""
+
+var imageHang4 = """
+    -------
+    T     T
+    🧟‍♀️    |
+    👋🏽🤚🏽  |
+    👞    |
+          |
+---------------------
+"""
+var imageHang5 = """
+    -------
+    T     T
+    🧟‍♀️    |
+    👋🏽🤚🏽  |
+   👞👞   |
+          |
+---------------------
+"""
+var imageHang6 = """
+    -------
+       |
+    💀💀💀
+  💀G A M E💀
+💀💀 O V E R 💀💀
+---------------------
+"""
+print (imageHang)
+sleep (2)
 
 let allTheWords = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake", "baby", "back", "balance", "ball", "band", "base", "basin", "basket", "bath", "beautiful", "because", "before", "behaviour", "belief", "bell", "bent", "berry", "between", "bird", "birth", "bite", "bitter", "black", "blade", "blood", "blow", "blue", "board", "boat", "body", "boiling", "bone", "book", "boot", "bottle", "brain", "brake", "branch", "brass", "bread", "breath", "brick", "bridge", "bright", "broken", "brother", "brown", "brush", "bucket", "building", "bulb", "burn", "burst", "business", "butter", "button", "cake", "camera", "canvas", "card", "care", "carriage", "cart", "cause", "certain", "chain", "chalk", "chance", "change", "cheap", "cheese", "chemical", "chest", "chief", "chin", "church", "circle", "clean", "clear", "clock", "cloth", "cloud", "coal", "coat", "cold", "collar", "colour", "comb", "come", "comfort", "committee", "common", "company", "comparison", "competition", "complete", "complex", "condition", "connection", "conscious", "control", "cook", "copper", "copy", "cord", "cork", "cotton", "cough", "country", "cover", "crack", "credit", "crime", "cruel", "crush", "current", "curtain", "curve", "cushion", "damage", "danger", "dark", "daughter", "dead", "dear", "death", "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction", "detail", "development", "different", "digestion", "direction", "dirty", "discovery", "discussion", "disease", "disgust", "distance", "distribution", "division", "door", "doubt", "down", "drain", "drawer", "dress", "drink", "driving", "drop", "dust", "early", "earth", "east", "edge", "education", "effect", "elastic", "electric", "engine", "enough", "equal", "error", "even", "event", "ever", "every", "example", "exchange", "existence", "expansion", "experience", "expert", "face", "fact", "fall", "false", "family", "farm", "father", "fear", "feather", "feeble", "feeling", "female", "fertile", "fiction", "field", "fight", "finger", "fire", "first", "fish", "fixed", "flag", "flame", "flat", "flight", "floor", "flower", "fold", "food", "foolish", "foot", "force", "fork", "form", "forward", "fowl", "frame", "free", "frequent", "friend", "from", "front", "fruit", "full", "future", "garden", "general", "girl", "give", "glass", "glove", "goat", "gold", "good", "government", "grain", "grass", "great", "green", "grey", "grip", "group", "growth", "guide", "hair", "hammer", "hand", "hanging", "happy", "harbour", "hard", "harmony", "hate", "have", "head", "healthy", "hear", "hearing", "heart", "heat", "help", "high", "history", "hole", "hollow", "hook", "hope", "horn", "horse", "hospital", "hour", "house", "humour", "idea", "important", "impulse", "increase", "industry", "insect", "instrument", "insurance", "interest", "invention", "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge", "land", "language", "last", "late", "laugh", "lead", "leaf", "learning", "leather", "left", "letter", "level", "library", "lift", "light", "like", "limit", "line", "linen", "liquid", "list", "little", "living", "lock", "long", "look", "loose", "loss", "loud", "love", "machine", "make", "male", "manager", "mark", "market", "married", "mass", "match", "material", "meal", "measure", "meat", "medical", "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed", "money", "monkey", "month", "moon", "morning", "mother", "motion", "mountain", "mouth", "move", "much", "muscle", "music", "nail", "name", "narrow", "nation", "natural", "near", "necessary", "neck", "need", "needle", "nerve", "news", "night", "noise", "normal", "north", "nose", "note", "number", "observation", "offer", "office", "only", "open", "operation", "opinion", "opposite", "orange", "order", "organization", "ornament", "other", "oven", "over", "owner", "page", "pain", "paint", "paper", "parallel", "parcel", "part", "past", "paste", "payment", "peace", "pencil", "person", "physical", "picture", "pipe", "place", "plane", "plant", "plate", "play", "please", "pleasure", "plough", "pocket", "point", "poison", "polish", "political", "poor", "porter", "position", "possible", "potato", "powder", "power", "present", "price", "print", "prison", "private", "probable", "process", "produce", "profit", "property", "prose", "protest", "public", "pull", "pump", "punishment", "purpose", "push", "quality", "question", "quick", "quiet", "quite", "rail", "rain", "range", "rate", "reaction", "reading", "ready", "reason", "receipt", "record", "regret", "regular", "relation", "religion", "representative", "request", "respect", "responsible", "rest", "reward", "rhythm", "rice", "right", "ring", "river", "road", "roll", "roof", "room", "root", "rough", "round", "rule", "safe", "sail", "salt", "same", "sand", "scale", "school", "science", "scissors", "screw", "seat", "second", "secret", "secretary", "seed", "seem", "selection", "self", "send", "sense", "separate", "serious", "servant", "shade", "shake", "shame", "sharp", "sheep", "shelf", "ship", "shirt", "shock", "shoe", "short", "shut", "side", "sign", "silk", "silver", "simple", "sister", "size", "skin", "skirt", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke", "smooth", "snake", "sneeze", "snow", "soap", "society", "sock", "soft", "solid", "some", "song", "sort", "sound", "soup", "south", "space", "spade", "special", "sponge", "spoon", "spring", "square", "stage", "stamp", "star", "start", "statement", "station", "steam", "steel", "stem", "step", "stick", "sticky", "stiff", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight", "strange", "street", "stretch", "strong", "structure", "substance", "such", "sudden", "sugar", "suggestion", "summer", "support", "surprise", "sweet", "swim", "system", "table", "tail", "take", "talk", "tall", "taste", "teaching", "tendency", "test", "than", "that", "then", "theory", "there", "thick", "thin", "thing", "this", "thought", "thread", "throat", "through", "through", "thumb", "thunder", "ticket", "tight", "till", "time", "tired", "together", "tomorrow", "tongue", "tooth", "touch", "town", "trade", "train", "transport", "tray", "tree", "trick", "trouble", "trousers", "true", "turn", "twist", "umbrella", "under", "unit", "value", "verse", "very", "vessel", "view", "violent", "voice", "waiting", "walk", "wall", "warm", "wash", "waste", "watch", "water", "wave", "weather", "week", "weight", "well", "west", "wheel", "when", "where", "while", "whip", "whistle", "white", "wide", "will", "wind", "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work", "worm", "wound", "writing", "wrong", "year", "yellow", "yesterday", "young"]
 
-
-var realDash = ""
+let alph = "abcdefghijklmnopqrstuvwxyz"
+var realDash = [String]()
 var chosenWords = ""
+var guessedLetters: [String] = []
+var randomWord = ""
+var counter = 0
+var game = true
 
-if let randomAll = allTheWords.randomElement() {
-    print(randomAll)
-    chosenWords = randomAll
-    for _ in 0...randomAll.count - 1 {
-realDash += "_ "
+repeat {
+
+    if let randomUn = allTheWords.randomElement() {
+        randomWord = randomUn
     }
-}
-print("What is your first guess? You have six attempts")
-let userResponse = readLine()
+    
+    let arrayWordChar = Array(randomWord)
+    var arrayWord = [String]()
+    for element in arrayWordChar {
+        arrayWord.append(String(element))
+    }
+    for _ in 0..<randomWord.count {
+        realDash.append("_ ")
+    }
+    
+    print()
+    print("What is your first guess? You have six attempts")
+    print(randomWord)
+    var numberofA = true
+    while numberofA {
+        for i in realDash {
+            print(i, separator: "", terminator: "")
+        }
+        print()
+        let attempt = readLine()!.lowercased()
+        if alph.contains(attempt) && attempt.count == 1 {
+            if randomWord.contains(attempt){
+                for i in 0..<arrayWord.count {
+                    if arrayWord[i] == attempt {
+                        realDash[i] = attempt
+                        if realDash == arrayWord {
+                            print("You win. Want to play again?")
+                            let winAnswer = readLine()
+                                if winAnswer == "yes" {
+                                print("")
+                                    realDash = []
+                                    randomWord = ""
+                                    counter = 0
+                                    numberofA = false
+                                    game = true
+                            } else {
+                                print("Bye")
+                            }
+                        }
+                        continue
+                    }
+                }
+            } else {
+                switch counter {
+                case 0:
+                    counter += 1
+                    print (imageHang1)
+                    print("\(counter) out of 6 strikes")
+                    print("try again")
+                case 1:
+                    counter += 1
+                    print (imageHang2)
+                    print("\(counter) out of 6 strikes")
+                    print("try again")
+                case 2:
+                    counter += 1
+                    print (imageHang3)
+                    print("\(counter) out of 6 strikes")
+                    print("try again")
+                case 3:
+                    counter += 1
+                    print (imageHang4)
+                    print("\(counter) out of 6 strikes")
+                    print("try again")
+                case 4:
+                    counter += 1
+                    print (imageHang5)
+                    print("\(counter) out of 6 strikes")
+                    print("try again")
+                case 5:
+                    counter += 1
+                    numberofA = false
+                    print (imageHang6)
+                    print("\(counter)th strike. Sorry, you lost. The word was \(randomWord)")
+                    print("Do you want to play again? Yes or No")
+                    let answer = (readLine())?.lowercased()
+                    if answer == "yes" {
+                        realDash = []
+                        randomWord = ""
+                        counter = 0
+                        numberofA = false
+                        game = true
+                        
+                    } else {
+                        game = false
+                        print("This game has ended.")
+                    }
+                default:
+                    print("are you even trying?")
+                }
+                
+            }
+        }
+    }
+    
+} while game
 
-var myArray = []
-print("type a , e, or i")
-if let input = readLine() {
-print(myArray.contains(input))
-}
